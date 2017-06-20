ac0f7f58f701102cd5c24756b635aeebf47bc42b
Bhalchandra <bhalchandra@BhalchadrasMBP2.fritz.box>
2017-06-20 19:27:57 +0200

    "Project configured to SpringBoot"

---
+++ Survey/src/main/java/spring/boot/Survey/Survey.java
@@ -0,0 +1,16 @@
+package spring.boot.Survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("spring.boot")
+public class Survey 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext appContext = SpringApplication.run(Survey.class, args);
+    }
+}
